b0f27f6e78ba4e5ce5b59fb3f4d7d687b5ec6b64
anask05 <168666362+anask05@users.noreply.github.com>
2024-05-04 21:30:33 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -2,7 +2,7 @@ name = 'Anar'
 print(name)
 age = "22"
 print(age)
-age_after_6_month = 22+1
-print(age_after_6_month)
+age_after_6_months = 22+1
+print(age_after_6_months)
 is_student = True
 print(is_student)
