764fc475ee9489181957978e117474cee5894aae
anask05 <168666362+anask05@users.noreply.github.com>
2024-05-05 22:18:51 +0300

    homework4.py

--- homework4.py
+++ homework4.py
@@ -6,6 +6,7 @@
 mutable_list = ['Anar', 22, 2001, 2024]
 print(mutable_list)
 mutable_list.append(True)
+mutable_list[0] = 'Askerov'
 mutable_list.remove(2001)
 mutable_list.extend([5])
 print(mutable_list)
