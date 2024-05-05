d0a126f2c5475e64f74925ef1093e3f56685cd7f
anask05 <168666362+anask05@users.noreply.github.com>
2024-05-05 22:08:09 +0300

    Неизменяемые и изменяемые объекты

---
+++ homework4.py
@@ -0,0 +1,10 @@
+#immutable_var = 'Anar', 22, 2001, 2024
+#print(immutable_var)
+#immutable_var[0][0] = 'Askerov'
+#print(immutable_var)
+
+mutable_list = ['Anar', 22, 2001, 2024]
+mutable_list.append(True)
+mutable_list.remove(2001)
+mutable_list.extend([5])
+print(mutable_list)
