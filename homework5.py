6b94da6fe1b45490bca47aff792f818de88672fe
anask05 <168666362+anask05@users.noreply.github.com>
2024-05-07 19:49:58 +0300

    Списки и словари

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['potato', 'tomato', 'cucumber', 'chilli', 'carrot']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'cabbage'
+print(my_list)
+
+my_dict = {'hand': "рука", 'leg': "нога", 'face': "лицо"}
+print(my_dict)
+print(my_dict["hand"])
+my_dict["eye"] = "глаз"
+print(my_dict)
