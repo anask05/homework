eeabcc808c520e149d26ace423cb04a576729ad7
anask05 <168666362+anask05@users.noreply.github.com>
2024-05-10 13:47:09 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,37 @@
+x = 40
+
+print('дарова')
+if x < 0:
+    print('меньше нуля')
+print('до свидания')
+
+a, b = 25, -5
+if a > b:
+    print('a > b')
+
+if a > b and a > 0:
+    print("успех")
+
+if (a > b) and (a > 0 or b < 1000):
+    print("успех")
+
+if 5 < b and b < 10:
+    print("успех")
+
+if '34' > '123':
+    print("ok")
+
+if '123' > '12':
+    print("ok")
+
+if [1, 2] > [1, 1]:
+    print("ok")
+
+if '6' > 5:
+    print("ok")
+
+if [5, 6] > 5:
+    print("ok")
+
+if '6' != 5:
+    print("ok")
