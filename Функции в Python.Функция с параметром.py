241072ebbee592797d8314c2876be3fa14327326
anask05 <168666362+anask05@users.noreply.github.com>
2024-05-12 14:19:25 +0300

    Add files via upload

---
+++ Функции в Python.Функция с параметром.py
@@ -0,0 +1,16 @@
+
+def print_params(name):
+    print("how r u", name)
+
+
+print_params("Kolya ?")
+print_params("Petya ?")
+print_params("Vasya ?")
+print_params("Maxim ?")
+
+# def f(arg):
+#     print(arg)
+#
+#
+# f("I believe in miracles")
+# f(666)
